0ae51691c1daeca6bf91513d448ab16b21f94088
Bill Wong <basilkhan.dev@gmail.com>
2017-02-05 21:23:06 -0500

    Create a Pipe Function to Enable Function Composition

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,8 @@
 export const partial = (fn, ...args) => fn.bind(null, ...args)
 
+const _pipe = (f, g) => (...args) => g(f(...args))
+
+export const pipe = (...fns) => fns.reduce(_pipe)
 // null to not chnage its context
 // 1st args ... multiple args and comma separated
 // 2nd args ... spread the the args 
